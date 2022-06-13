249767f28ca3de9ba43d82cd36195ee3bd0e33b2
HernandezDev <hernandezfmb@gmail.com>
2022-06-13 17:55:51 -0400

    adding redirection

--- redirect.js
+++ redirect.js
@@ -5,4 +5,5 @@ const urlParams = new URLSearchParams(keyValues);
 const _id = urlParams.get('_id');
 const recoveryURL = urlParams.get('recoveryURL');
 
-console.log(`${recoveryURL}/${_id}`);
+
+window.location.replace(`${recoveryURL}/${_id}`);
